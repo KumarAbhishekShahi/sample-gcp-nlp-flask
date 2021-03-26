7c85d0d6f344786064c316f611f09aa1eb3eb363
abhishek.shahi@gmail.com <abhishek.shahi@gmail.com>
2021-03-26 09:33:14 +0000

    updated

--- language_api/main-final.py
+++ language_api/main-final.py
@@ -33,11 +33,16 @@ def upload_text():
     text = request.form["text"]
 
     # Analyse sentiment using Sentiment API call
-    #sentiment = analyze_text_sentiment(text)[0].get('sentiment score')
-    sentiment = gcp_analyze_syntax(text)[0].get('Label')
+    sentiment = analyze_text_sentiment(text)[0].get('sentiment score')
 
     # Assign a label based on the score
-    #removed something
+    overall_sentiment = 'unknown'
+    if sentiment > 0:
+        overall_sentiment = 'positive'
+    if sentiment < 0:
+        overall_sentiment = 'negative'
+    if sentiment == 0:
+        overall_sentiment = 'neutral'
 
     # Create a Cloud Datastore client.
     datastore_client = datastore.Client()
@@ -59,7 +64,7 @@ def upload_text():
     entity = datastore.Entity(key)
     entity["text"] = text
     entity["timestamp"] = current_datetime
-    entity["sentiment"] = sentiment
+    entity["sentiment"] = overall_sentiment
 
     # Save the new entity to Datastore.
     datastore_client.put(entity)
@@ -106,83 +111,6 @@ def analyze_text_sentiment(text):
 
     return sentence_sentiment
 
-# Syntax Analysis
-def gcp_analyze_syntax(text, debug=0):
-    """
-    Analyzing Syntax in a String
-
-    Args:
-      text The text content to analyze
-    """
-
-    client = language.LanguageServiceClient()
-    document = language.Document(content=text, type_=language.Document.Type.PLAIN_TEXT)
-    response = client.analyze_syntax(document=document)
-    
-    output = []   
-    # Loop through tokens returned from the API
-    for token in response.tokens:
-        word = {}
-        # Get the text content of this token. Usually a word or punctuation.
-        text = token.text  
-
-        # Get the part of speech information for this token.
-        # Parts of spech are as defined in:
-        # http://www.lrec-conf.org/proceedings/lrec2012/pdf/274_Paper.pdf
-        part_of_speech = token.part_of_speech
-        # Get the tag, e.g. NOUN, ADJ for Adjective, et al.
-        
-        # Get the dependency tree parse information for this token.
-        # For more information on dependency labels:
-        # http://www.aclweb.org/anthology/P13-2017
-        dependency_edge = token.dependency_edge   
-        
-        word["word"]=text.content
-        word["begin_offset"]=text.begin_offset        
-        word["part_of_speech"]=language.PartOfSpeech.Tag(part_of_speech.tag).name
-        
-        # Get the voice, e.g. ACTIVE or PASSIVE
-        word["Voice"]=language.PartOfSpeech.Voice(part_of_speech.voice).name
-        word["Tense"]=language.PartOfSpeech.Tense(part_of_speech.tense).name
-        
-        # See API reference for additional Part of Speech information available
-        # Get the lemma of the token. Wikipedia lemma description
-        # https://en.wikipedia.org/wiki/Lemma_(morphology)        
-        word["Lemma"]=token.lemma
-        word["index"]=dependency_edge.head_token_index
-        word["Label"]=language.DependencyEdge.Label(dependency_edge.label).name
-        
-        if debug:
-            print(u"Token text: {}".format(text.content))
-            print(
-                u"Location of this token in overall document: {}".format(text.begin_offset)
-            ) 
-            print(
-                u"Part of Speech tag: {}".format(
-                    language.PartOfSpeech.Tag(part_of_speech.tag).name
-                )
-            )        
-
-            print(u"Voice: {}".format(language.PartOfSpeech.Voice(part_of_speech.voice).name))
-            # Get the tense, e.g. PAST, FUTURE, PRESENT, et al.
-            print(u"Tense: {}".format(language.PartOfSpeech.Tense(part_of_speech.tense).name))
-
-            print(u"Lemma: {}".format(token.lemma))
-
-            print(u"Head token index: {}".format(dependency_edge.head_token_index))
-            print(
-                u"Label: {}".format(language.DependencyEdge.Label(dependency_edge.label).name)
-            )
-        
-        output.append(word)
-        
-
-    # Get the language of the text, which will be the same as
-    # the language specified in the request or, if not specified,
-    # the automatically-detected language.
-    if debug:
-        print(u"Language of the text: {}".format(response.language))
-    return (output)
 
 if __name__ == "__main__":
     # This is used when running locally. Gunicorn is used to run the
